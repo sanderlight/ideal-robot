7c57005e69f4378cc2575cf73c836ee30d6fd55a
sanderlight <116835755+sanderlight@users.noreply.github.com>
2022-10-27 15:26:55 -0400

    Add files via upload

---
+++ src/pages/_document.tsx
@@ -0,0 +1,14 @@
+import {Head, Html, Main, NextScript} from 'next/document';
+
+const document = () =>{
+ return(
+    <Html className="dark">
+        <Head/>
+        <body>
+            <Main />
+            <NextScript />
+        </body>
+    </Html>
+ );
+};export default document
+
---
+++ src/utils/bin/sumfetch.ts
@@ -0,0 +1,47 @@
+import config from '../../../config.json';
+
+const sumfetch = async (args: string[]): Promise<string> => {
+  if (config.ascii === 'cveinnt') {
+    return `                                                  
+             @@@@@@@@@@@@@                   sumfetch: summary display
+        @@@@               @@@@             -----------
+      @@                       @@            ABOUT
+    @@                           @@          ${config.name}
+  @@                               @@        ${config.ps1_hostname}
+ @@                         @@@     @@       <u><a href="${config.resume_url}" target="_blank">resume</a></u>
+@@        @@@                        @@      <u><a href="${config.repo}" target="_blank">Github repo</a></u>
+@@                                   @@     -----------
+@@             .@@@@@@@@@@.          @@      CONTACT 
+ @@           @@          @@        @@       <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
+  @@           @@        @@        @@        <u><a href="https://youtube.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
+   @@             @@@@@@          @@         <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
+     @@@                        @@@         -----------
+        @@@                  @@@ @@          DONATE 
+         @|  @@@@@@@@@@@@@@@@   @@           <u><a href="${config.donate_urls.paypal}" target="_blank">${config.donate_urls.paypal}</a></u>
+         @|                <u><a href="${config.donate_urls.patreon}" target="_blank">${config.donate_urls.patreon}</a></u>
+
+`;
+  } else {
+    return `
+           ▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▄                  BIO
+        ▄▓▓▀ ▄▓▓▀▓▓▓▀▓▓▄ ▀▀▓▓▄              -----------
+      ▓▓▀  ▄▓▀   ▐▓▓  ▀▓▓    ▓▓▄             ABOUT
+    ▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓            ${config.name}
+   ▓▓     ▓▓▓    ▐▓▓    ▐▓▓     ▓▓          <P> </P>
+▐▓▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▓       
+▐▓                                 ▐▓       -----------
+▐▓        > @SANDERLIGHT1          ▐▓        CONTACT 
+▐▓                                 ▐▓        <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
+▐▓▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▓         <u><a href="https://www.youtube.com/channel/UCqVbF8WpWSbdRgKzwnJHqdw" target="_blank"> CLICK ME TO WIN MONEY/${config.social.YOUTUBE}</a></u>
+                                              <u><a href="https://www.tiktok.com/@sanderlight1" target="_blank"> NO CLICK ME/${config.social.TIKTOK}</a></u>
+   ▓▓      ▐▓▓    ▓▓    ▐▓▓     ▓▓           <u><a href="https://www.instagram.com/sanderlight1/" target="_blank"> TIKTOK.com/in/${config.social.INSTAGRAM}</a></u>
+    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓           -----------
+      ▓▓▓   ▐▓▓   ▓▓   ▓▓▓   ▓▓▀             DONATE 
+        ▀▓▓▄▄ ▀▓▓▄▓▓▄▓▓▓▄▄▓▓▀                <u><a href="${config.donate_urls.paypal}" target="_blank">${config.donate_urls.paypal}</a></u>
+            ▀▓▓▓▓▓▓▓▓▓▓▓▀▀                   <u><a href="${config.donate_urls.patreon}" target="_blank">${config.donate_urls.patreon}</a></u>
+
+`;
+  }
+};
+
+export default sumfetch;
